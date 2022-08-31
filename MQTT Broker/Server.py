72922b5453f40285936c3ad7f37c2f54098fe200
Daniel Sørensen <108401702+OleHeie@users.noreply.github.com>
2022-08-31 08:43:04 +0200

    Update

--- MQTT Broker/Server.py
+++ MQTT Broker/Server.py
@@ -1,5 +1,11 @@
+from base64 import decode, encode
+from codecs import utf_7_encode
+from http import client
+from itertools import tee
+from pydoc import cli
+from queue import PriorityQueue
 from MQTTService import c_MQTTService
-
+from MQTTHelper import c_MQTTHelper
 import socket
 from _thread import *
 class c_Server:
@@ -8,28 +14,50 @@ class c_Server:
     ThreadCount = 0
     clients = list()
     MQTTService = c_MQTTService()
-
+    helper = c_MQTTHelper()
     def __init__(self):
         self.start_server(self.host, self.port)
 
     def client_handler(self,connection):
         self.clients.append(connection)
-        connection.send(str.encode('You are now connected to the replay server... Type BYE to stop'))
+        # connection.send(str.encode('You are now connected to the replay server... Type BYE to stop'))
         while True:
             try:
+               
+             
+              
+                
                 data = connection.recv(2048)
-                print("Data under:")
-                print(data)
-                print("end of data")
-                self.MQTTService.ValidateConnect(data)
-                message = data.decode('utf-8')
-                print(message)
-                if message == 'BYE':
-                    break
-                reply = f'Server: {message}'
-                connection.sendall(str.encode(reply))
-            except :
-                connection.close()
+               
+                # connection.sendall(data)
+
+                print(b'\x20\x02\x00\x00')
+                print(type(b'\0x20\0x02\0x00\0x00'))
+                connection.sendall(b'\x20\x02\x00\x00')
+                
+                # print("Data under:")
+                # print(data)
+                # print("end of data")
+                # # self.MQTTService.ValidateConnect(data)
+                # hexarray = self.helper.ConvertDecimalToHex(data)
+                # #utf form
+            
+                # message = data.decode('utf-8')
+                
+                # print()
+                # # teem =  self.MQTTService.
+                # (hexarray[0])
+                 
+                # teem2 = "\0x20\0x02\0x00\0x00\0x00"
+            
+                # if message == 'BYE':
+                #     break
+                # reply = f'Server: {message}'
+                # connection.sendall(str.encode(teem2))
+            except error as err:
+                print(err)
+                # connection.close()
+                # connection.close()
             
 
     def accept_connections(self, ServerSocket):
